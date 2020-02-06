dc6cf70d078fd13914e67018a56a2bca9d1b1352
Nmatt1 <matthews3102000@gmail.com>
2020-02-06 13:30:18 +0000

    Add files via upload

---
+++ is this an API/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+
+namespace is_this_an_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strurltest = String.Format("https://inara.cz/");
+            WebRequest requestObjGet = WebRequest.Create(strurltest);
+            string response = requestObjGet.GetResponse().ToString();
+            Console.WriteLine(response);
+            Console.ReadLine();
+
+        }
+    }
+}
